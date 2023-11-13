4e942077d707884ca21c8e6942664526efa20f15
Mr-Sahil-Sharma <sahilatfau@gmail.com>
2023-11-13 16:59:52 +0100

    exercise1.py updated

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,2 +1,34 @@
-from sqlalchemy import create_engine, Integer, Text, Float
+import pandas as pd
+from sqlalchemy import create_engine, Integer, Float, Text
 
+#URL of the CSV File:
+url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
+
+#Read the CSV File:
+data = pd.read_csv(url, sep=';')
+
+# Define Datatypes:
+column_DataTypes = {
+    "column_1": Integer(),
+    "column_2": Text(),
+    "column_3": Text(),
+    "column_4": Text(),
+    "column_5": Text(),
+    "column_6": Text(),
+    "column_7": Float(),
+    "column_8": Float(),
+    "column_9": Integer(),
+    "column_10": Float(),
+    "column_11": Text(),
+    "column_12": Text(),
+    "geo_punkt": Float()
+}
+
+#Connection to the Database:
+engine = create_engine("sqlite:///airports.sqlite")
+
+# Store the data in airports table:
+data.to_sql("airports", engine, index=False, dtype=column_DataTypes, if_exists="replace")
+
+#Connection close
+engine.dispose()

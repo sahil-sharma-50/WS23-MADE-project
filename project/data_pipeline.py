942121033fd1473fe4a574654df4d778b10fac37
Sahil Sharma <sahilatfau@gmail.com>
2023-11-22 17:36:12 +0100

    Pipeline Updated

--- project/data_pipeline.py
+++ project/data_pipeline.py
@@ -16,23 +16,14 @@ def preprocess_bike_traffic(bike_data):
     bike_data = bike_data.drop(['uhrzeit_start', 'uhrzeit_ende', 'zaehlstelle', 'richtung_1', 'richtung_2'], axis=1)
     bike_data = bike_data.rename(columns={'datum': 'Date', 'gesamt': 'Total'})
     bike_data['Date'] = pd.to_datetime(bike_data['Date'], format='%Y.%m.%d').dt.strftime('%d.%m.%Y')
-    bike_data['Date'] = pd.to_datetime(bike_data['Date'], format='%d.%m.%Y')
     bike_data = bike_data.groupby('Date')['Total'].sum().reset_index()
     return bike_data
 
 def main():
-    # Load weather data
     munich_weather = load_data("munich_weather/history_weather_munich.csv")
-
-    # Load bike traffic data
     bike_traffic = load_data("bike_traffic/rad_15min.csv", date_column_format='%Y.%m.%d')
-
-    # Preprocess weather data for a specific year
     munich_2017 = preprocess_weather(munich_weather, year=2017)
-
-    # Preprocess bike traffic data
     preprocessed_bike_traffic = preprocess_bike_traffic(bike_traffic)
-
     bike_traffic_filtered = preprocessed_bike_traffic[preprocessed_bike_traffic['Date'] < '2017-05-31']
 
 if __name__ == "__main__":
